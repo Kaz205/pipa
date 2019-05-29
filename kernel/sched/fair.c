63c713390ac2bb9ab75125ef60edb9849f664ed6
Peter Zijlstra <peterz@infradead.org>
2019-05-29 20:36:44 +0000

    UPSTREAM: sched: Rework pick_next_task() slow-path
    (cherry picked from commit 67692435c411e)
    
    Avoid the RETRY_TASK case in the pick_next_task() slow path.
    
    By doing the put_prev_task() early, we get the rt/deadline pull done,
    and by testing rq->nr_running we know if we need newidle_balance().
    
    This then gives a stable state to pick a task from.
    
    Since the fast-path is fair only; it means the other classes will
    always have pick_next_task(.prev=NULL, .rf=NULL) and we can simplify.
    
    BUG=b:152605392
    TEST=run power_VideoCall test
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Aaron Lu <aaron.lwe@gmail.com>
    Cc: Valentin Schneider <valentin.schneider@arm.com>
    Cc: mingo@kernel.org
    Cc: Phil Auld <pauld@redhat.com>
    Cc: Julien Desfossez <jdesfossez@digitalocean.com>
    Cc: Nishanth Aravamudan <naravamudan@digitalocean.com>
    Link: https://lkml.kernel.org/r/aa34d24b36547139248f32a30138791ac6c02bd6.1559129225.git.vpillai@digitalocean.com
    Signed-off-by: Joel Fernandes <joelaf@google.com>
    Change-Id: I2029a0b9994aeddf83248bf5da67c93f2290f5e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2186288
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8325,7 +8325,7 @@ again:
 		goto idle;
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
-	if (prev->sched_class != &fair_sched_class)
+	if (!prev || prev->sched_class != &fair_sched_class)
 		goto simple;
 
 	/*
@@ -8402,8 +8402,8 @@ again:
 	goto done;
 simple:
 #endif
-
-	put_prev_task(rq, prev);
+	if (prev)
+		put_prev_task(rq, prev);
 
 	do {
 		se = pick_next_entity(cfs_rq, NULL);
@@ -8431,6 +8431,9 @@ done: __maybe_unused;
 	return p;
 
 idle:
+	if (!rf)
+		return NULL;
+
 	new_tasks = newidle_balance(rq, rf);
 
 	/*
