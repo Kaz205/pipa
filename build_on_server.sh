b9f44f8c1618dcaf9084478e18afb1b0fb16bdb7
Kazuki Hashimoto <kazukih0205@gmail.com>
2023-08-25 10:20:02 +0900

    Add build scripts

---
+++ build_on_server.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+build_mode=$1
+echo $build_mode | grep -q "clean\|dirty"
+if [ $? = 1 ]
+then
+    echo "Please specify whether to clean build or dirty build"
+    exit 1
+fi
+
+if [ $build_mode = "clean" ]
+then
+    ssh kazuki@192.168.13.229 "rm -rf /home/kazuki/pipa/out"
+fi
+
+# Build
+ssh -tt kazuki@192.168.13.229 "cd pipa && git pull origin && git reset --hard FETCH_HEAD && bash build_kernel.sh"
+if [ $? != 0 ]
+then
+    echo $?
+    echo -e "\e[0;31mBuild failed, exiting... \e[0m"
+    exit 1
+fi
+
+# SSH to server then list zips by date then cut and parse zip name
+server_file=$(ssh kazuki@192.168.13.229 "cd /home/kazuki/AnyKernel3 && ls -altr" | rg zip | tail -1 - | awk '{print $NF}')
+
+scp kazuki@192.168.13.229:/home/kazuki/AnyKernel3/$server_file ~/AnyKernel3/$server_file
