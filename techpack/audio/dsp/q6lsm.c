30042d1a84ffc8436b4c309b1bbd9d8e0fb33bf7
Kazuki H <kazukih0205@gmail.com>
2023-09-02 18:29:03 +0900

    techpack: audio: Enable some missed pipa quirks

--- techpack/audio/dsp/q6lsm.c
+++ techpack/audio/dsp/q6lsm.c
@@ -1899,9 +1899,7 @@ int q6lsm_deregister_sound_model(struct lsm_client *client)
 		pr_err("%s: session[%d]", __func__, client->session);
 		return -EINVAL;
 	}
-#if defined(CONFIG_TARGET_PRODUCT_PIPA)
 	client->model_reged = false;
-#endif
 	memset(&cmd, 0, sizeof(cmd));
 	q6lsm_add_hdr(client, &cmd.hdr, sizeof(cmd.hdr), false);
 	cmd.hdr.opcode = LSM_SESSION_CMD_DEREGISTER_SOUND_MODEL;
