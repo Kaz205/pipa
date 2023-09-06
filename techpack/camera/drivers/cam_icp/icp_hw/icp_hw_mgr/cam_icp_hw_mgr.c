a965de092bc0b56c1124af0997368f491f135214
Kazuki H <kazukih0205@gmail.com>
2023-09-07 05:30:40 +0900

    camera: Import pipa changes

--- techpack/camera/drivers/cam_icp/icp_hw/icp_hw_mgr/cam_icp_hw_mgr.c
+++ techpack/camera/drivers/cam_icp/icp_hw/icp_hw_mgr/cam_icp_hw_mgr.c
@@ -2523,10 +2523,7 @@ static int cam_icp_mgr_process_fatal_error(
 
 	if (event_notify->event_id == HFI_EVENT_SYS_ERROR) {
 		CAM_INFO(CAM_ICP, "received HFI_EVENT_SYS_ERROR");
-		if (event_notify->event_data1 == HFI_ERR_SYS_FATAL) {
-			CAM_ERR(CAM_ICP, "received HFI_ERR_SYS_FATAL");
-			BUG();
-		} else if (event_notify->event_data1 ==
+		if (event_notify->event_data1 ==
 			HFI_ERR_SYS_RESET_FAILURE) {
 			for (i = 0; i < CAM_ICP_CTX_MAX; i++) {
 				if (hw_mgr->ctx_data[i].state !=
