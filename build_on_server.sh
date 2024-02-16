6be6fa7bc9f130f01cd3a8eff98677f48f2428af
Kazuki H <kazukih0205@gmail.com>
2024-02-17 07:53:48 +0900

    build_on_server: Update server address

--- build_on_server.sh
+++ build_on_server.sh
@@ -10,15 +10,15 @@ fi
 
 if [ $build_mode = "clean" ]
 then
-    ssh kazuki@192.168.13.229 "rm -rf /home/kazuki/pipa/out"
+    ssh kazuki@192.168.1.229 "rm -rf /home/kazuki/pipa/out"
 fi
 
 # Checkout
 current_branch=$(git rev-parse --abbrev-ref HEAD)
-ssh -tt kazuki@192.168.13.229 "cd pipa && git pull origin && git reset --hard FETCH_HEAD && git checkout $current_branch"
+ssh -tt kazuki@192.168.1.229 "cd pipa && git pull origin && git reset --hard FETCH_HEAD && git checkout $current_branch"
 
 # Build
-ssh -tt kazuki@192.168.13.229 "cd pipa && bash build_kernel.sh"
+ssh -tt kazuki@192.168.1.229 "cd pipa && bash build_kernel.sh"
 if [ $? != 0 ]
 then
     echo $?
@@ -27,6 +27,6 @@ then
 fi
 
 # SSH to server then list zips by date then cut and parse zip name
-server_file=$(ssh kazuki@192.168.13.229 "cd /home/kazuki/AnyKernel3 && ls -altr" | rg zip | tail -1 - | awk '{print $NF}')
+server_file=$(ssh kazuki@192.168.1.229 "cd /home/kazuki/AnyKernel3 && ls -altr" | rg zip | tail -1 - | awk '{print $NF}')
 
-scp kazuki@192.168.13.229:/home/kazuki/AnyKernel3/$server_file ~/AnyKernel3/$server_file
+scp kazuki@192.168.1.229:/home/kazuki/AnyKernel3/$server_file ~/AnyKernel3/$server_file
